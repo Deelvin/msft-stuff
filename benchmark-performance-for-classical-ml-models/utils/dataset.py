0caf9e99685035dcb31e24907941cf53b740aaab
KJlaccHoeUM9l <wotpricol@mail.ru>
2022-11-18 15:39:24 +0300

    update dataset config

--- benchmark-performance-for-classical-ml-models/utils/dataset.py
+++ benchmark-performance-for-classical-ml-models/utils/dataset.py
@@ -6,10 +6,10 @@ import sklearn.datasets
 
 def get_classification_dataset() -> typing.Tuple[numpy.ndarray, numpy.ndarray]:
     X, y = sklearn.datasets.make_classification(
-        n_samples=10,
-        n_features=224 * 224 * 3,
-        n_classes=1000,
-        n_informative=11,
+        n_samples=10000,
+        n_features=500,
+        n_classes=100,
+        n_informative=8,
         random_state=47,
         shuffle=False,
     )
@@ -18,6 +18,6 @@ def get_classification_dataset() -> typing.Tuple[numpy.ndarray, numpy.ndarray]:
 
 def get_regression_dataset() -> typing.Tuple[numpy.ndarray, numpy.ndarray]:
     X, y = sklearn.datasets.make_regression(
-        n_samples=10, n_features=100000, n_targets=1000, random_state=47, shuffle=False
+        n_samples=10000, n_features=500, n_targets=1, random_state=47, shuffle=False
     )
     return X.astype("float32"), y
