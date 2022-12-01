9a417359e24cb2e97da8d7f7dc8e664beac7d725
Valery Chernov <valery.chernov@deelvin.com>
2022-12-01 19:20:42 +0300

    fix str-Path issue

--- benchmark-hugging-face/meta_utils.py
+++ benchmark-hugging-face/meta_utils.py
@@ -1,10 +1,15 @@
 from pathlib import Path
+from typing import Union
 
 
 MODULE_EQUALITY="ignore-ndarray"
 
-def get_workload_path(dir: Path, name: str):
+def get_workload_path(dir: Union[str, Path], name: str):
+  if isinstance(dir, str):
+    dir = Path(dir)
   return str(dir.joinpath( name + "_workload.json"))
 
-def get_record_path(dir: Path, name: str):
+def get_record_path(dir: Union[str, Path], name: str):
+  if isinstance(dir, str):
+    dir = Path(dir)
   return str(dir.joinpath( name + "_records.json"))
